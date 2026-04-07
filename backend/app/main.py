f612f8a4b9a9334ad94b821ab79153ee5599605a
Nitishkumar Dharmendran <nitishkumarofficial19@gmail.com>
2026-04-07 15:56:11 +0530

    fixvercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://data-forge-workspace-fhtw.vercel.app"
+        "https://data-forge-workspace-7vh7.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
